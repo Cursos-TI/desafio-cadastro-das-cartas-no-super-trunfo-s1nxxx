870619d5e72eaa8ce4e6146618feb8d42dcb956b
s1nxxx <damatta02@gmail.com>
2025-05-15 17:36:10 +0000

    Criei o printf inicial

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,10 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    
+    printf("Cartas SuperTrunfo\n");
+
+
+    
     return 0;
 }
